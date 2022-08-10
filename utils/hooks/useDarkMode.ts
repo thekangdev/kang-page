d32fc2dbb84f75bbd8903511e580424d67e92b66
ShivamBh <shivam.bhattacharjee94@gmail.com>
2022-08-10 18:22:19 +0530

    create useDarkMode hook

---
+++ utils/hooks/useDarkMode.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+function useDarkMode() {
+  const [theme, setTheme] = useState("light");
+  const colorTheme = theme === "dark" ? "light" : "dark";
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove(colorTheme);
+    root.classList.add(theme);
+
+    if (typeof window !== "undefined") {
+      localStorage.setItem("theme", theme);
+    }
+  }, [theme]);
+
+  return [colorTheme, setTheme] as const;
+}
+
+export default useDarkMode;
